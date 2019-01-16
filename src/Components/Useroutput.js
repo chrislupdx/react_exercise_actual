a750ece67bcec4cf1094edb925f623cdc367e42c
chrislupdx <christopherlu.pdx@gmail.com>
2019-01-16 08:52:16 -0800

    apparently my functional component doesn't need react.fragment to work?

--- src/Components/Useroutput.js
+++ src/Components/Useroutput.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const useroutput = (props) => {
     return (
-    <React.Fragment>
-    <p> {props.name} THERE ARE TOO MANY CHEESE HERE IN THIS HOUSEHOLD</p>  
+        <div>
+        <p> {props.name} THERE ARE TOO MANY CHEESE HERE IN THIS HOUSEHOLD</p>      
+        <p>IN DIESEM HAUS GIBT ES NICHT GENUG KÄSE </p>
+        </div>
     
-    <p>IN DIESEM HAUS GIBT ES NICHT GENUG KÄSE </p>
-    </React.Fragment>
 );
 };
 
